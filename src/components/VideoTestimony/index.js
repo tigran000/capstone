d8d85a1a78ba951537f74f676d503aa6e6af407e
Tigran <tigranharutynyan59@gmailcom>
2019-05-16 20:57:26 +0400

    adding video

--- src/components/VideoTestimony/index.js
+++ src/components/VideoTestimony/index.js
@@ -1,5 +1,15 @@
 import React from "react"
-
-const VideoTestimony = () => <h1> VideoTestimony </h1>
+import "./VideoTestimony.css"
+const VideoTestimony = () => (
+  <div className="vidios-container">
+    <iframe
+      title="capstone"
+      src="https://www.youtube.com/embed/HPI9NjMxq_A"
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
+  </div>
+)
 
 export default VideoTestimony
